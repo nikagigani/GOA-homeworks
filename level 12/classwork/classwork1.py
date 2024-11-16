4f2732c617cf9fecef29845f814875e39a3991dd
nikagigani <giganinika7@gmail.com>
2024-11-16 14:22:26 +0400

    commit

---
+++ level 12/classwork/classwork1.py
@@ -0,0 +1,9 @@
+print(True or True)#True
+print(False or False)#False
+print(True or False)#True
+print(False or True)#True
+
+print(True and True)#True
+print(False and False)#False
+print(True and False)#False
+print(False and True)#False
---
+++ level 12/classwork/classwork2.py
@@ -0,0 +1,4 @@
+num1=int(input("enter number: "))
+num2=int(input("enter number: "))
+print(num1>30 and num2<40)
+print(num1<35 and num2>60)
