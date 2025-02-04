02b55a975bb2c4d64594e39431b59cb6bfec4918
nikagigani <giganinika7@gmail.com>
2025-02-04 22:49:38 +0400

    commit

---
+++ level 36/classwork/classwork1.py
@@ -0,0 +1,46 @@
+def multiply(a, b):
+    return a * b
+
+
+def even_or_odd(number):
+    if number %2 == 0:
+        return "Even"
+    else: return "Odd"
+
+
+def number_to_string(num):
+    return str(num)
+
+
+def solution(string):
+    return string[::-1]
+
+
+def make_negative( number ):
+    if number > 0:
+        return number * -1
+    else: return number
+
+
+def opposite(number):
+    return number * -1
+
+
+def bool_to_word(boolean):
+    if boolean==True:
+        return "Yes"
+    else:
+        return "No"
+
+
+def positive_sum(arr):
+    res = 0
+    for i in arr:
+        if i > 0:
+            res += i
+    return res
+
+
+def repeat_str(repeat, string):
+    return string * repeat
+
